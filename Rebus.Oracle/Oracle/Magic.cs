334ba75a744f03e017c47867fc3fe43df6c0dbc1
jods4 <joel.dumas@elca.ch>
2019-08-17 14:29:14 +0200

    Remove obsolete code

--- Rebus.Oracle/Oracle/Magic.cs
+++ Rebus.Oracle/Oracle/Magic.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
 using Oracle.ManagedDataAccess.Types;
 
@@ -7,26 +6,6 @@ namespace Rebus.Oracle
 {
     static class OracleMagic
     {
-        public static List<string> GetTableNames(this OracleDbConnection connection)
-        {
-            var tableNames = new List<string>();
-
-            using (var command = connection.CreateCommand())
-            {
-                command.CommandText = "select table_name from USER_TABLES";
-
-                using (var reader = command.ExecuteReader())
-                {
-                    while (reader.Read())
-                    {
-                        tableNames.Add(reader["table_name"].ToString());
-                    }
-                }
-            }
-
-            return tableNames;
-        }
-
         public static DateTimeOffset ToDateTimeOffset(this OracleTimeStampTZ value)
         {
             return new DateTimeOffset(value.Value, value.GetTimeZoneOffset());
